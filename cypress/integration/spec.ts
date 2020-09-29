485a36c9f65e5ca5c9b207ec6a489b207c538c94
Konstantin Denerz <konstantin.denerz@gmail.com>
2020-09-29 14:07:42 +0200

    test(app): enhance integration test

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,5 +1,9 @@
-it("loads examples", () => {
-  const baseUrl: string = "http://localhost:4200";
+it('loads examples', () => {
+  const baseUrl = 'http://localhost:4200';
   cy.visit(baseUrl);
-  cy.contains("Replace me with something relevant");
+  cy.url().should('eq', `${baseUrl}/`);
+  cy.contains('.highlight-card > span', 'angular-cypress-new-project');
+  cy.contains('.terminal', 'ng generate component xyz');
+  cy.get(':nth-child(8) > :nth-child(2)').click();
+  cy.contains('.terminal', 'ng add @angular/material');
 });
